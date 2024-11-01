39dfa13a75eea8c65de9856e725923c9ff467037
Tyron <tyron@Tyrons-Mac-mini.local>
2024-10-31 22:49:43 -0700

    Added 404 page, mobile updates

---
+++ 404.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The uh oh page
+*/
+get_header();
+?>
+	<main id="four0four_wrapper">
+        <div id="four_image">
+            <h1>404</h1>
+            <p>Oh no, page not found! <a href="<?php bloginfo('url'); ?>">Take me home.</a></p>
+        </div>
+	</main><!-- #main -->
+
+<?php
+get_footer();
+?>
